f013157b2d999ce577f63b8ea8deab9cffa2fe92
Hosim33 <63397914+Hosim33@users.noreply.github.com>
2020-09-23 23:36:43 +0900

    Add files via upload

---
+++ signup_2.php
@@ -0,0 +1,8 @@
+<h1><strong>회원가입이 완료되었습니다.</strong></h1>
+<h2>고객님이 입력하신 정보는 아래와 같습니다.</h2>
+<?php $id = $_POST['id']; $pw = $_POST['pw'];
+$nm = $_POST['nm']; $gender = $_POST['s'];
+echo "로그인 아이디 : $id<BR>";
+echo "비밀번호 : $pw<BR>";
+echo "이름 : $nm<BR>";
+echo "성별 : $gender<BR>"; ?>
